d7ca7e42f7de2d89dc423cf1d3e0fc633ef4b1af
anirudh <anirudh@nazi.com>
2018-02-10 11:08:08 +0000

    my friends details

--- barcode/myscript.js
+++ barcode/myscript.js
@@ -3,7 +3,7 @@ var list
 // The initial method called on page load
 function init() {
     // load details from data file
-    loadDataAndParse('ShoppingItems.json')
+    loadDataAndParse('students.json')
 }
 
 function getItemWithBarCode(event) {
@@ -59,7 +59,7 @@ function loadContents(fileName, callback){
 //   -1 when not found
 function findWithBarcode(barcode) {
     for (var i = 0; i < list.length; i++) {
-        if (barcode == list[i].id) {
+        if (barcode == list[i].roll_no) {
             // found the item
             return i
         }
@@ -69,13 +69,12 @@ function findWithBarcode(barcode) {
 
 function detailsOfItem(index) {
     
-    var details = "id: " + list[index].id
+    var details = "Roll Number: " + list[index].roll_no
     details += "<br>"
-    details += list[index].item
+    details += list[index].name
     details += "<br>"
-    details += list[index].description
+    details += list[index].aadhar
     details += "<br>"
-    details += "Rs. " + list[index].price
     
     return details
 }
